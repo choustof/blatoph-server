a213b8a3ef59b51730ef05822e7ea41da0faf379
choustof <petit.christophe.91@gmail.com>
2017-04-09 11:36:12 +0200

    Gestion erreurs 404 (GET)

--- src/AppBundle/Controller/UtilisateurController.php
+++ src/AppBundle/Controller/UtilisateurController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -6,54 +7,54 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\Utilisateur;
 
-class UtilisateurController extends Controller
-{
+class UtilisateurController extends Controller {
 	/**
-     * @Route("/utilisateurs", name="utilisateurs_list")
-     * @Method({"GET"})
-     */
-    public function getUtilisateursAction(Request $request)
-    {
-        $utilisateurs = $this->get('doctrine.orm.entity_manager')
-                ->getRepository('AppBundle:Utilisateur')
-                ->findAll();
-        /* @var $utilisateurs Utilisateur[] */
-
-        $formatted = [];
-        foreach ($utilisateurs as $utilisateur) {
-            $formatted[] = [
-               'uti_id' => $utilisateur->getId(),
-               'uti_nom' => $utilisateur->getNom(),
-            	'uti_prenom' => $utilisateur->getPrenom(),
-            	'uti_email' => $utilisateur->getEmail(),
-           		'uti_mot_de_pass' => $utilisateur->getMotDePass(),
-            ];
-        }
-
-        return new JsonResponse($formatted);
-    }
-    
-    /**
-     * @Route("/utilisateurs/{uti_id}", name="utilisateurs_one")
-     * @Method({"GET"})
-     */
-    public function getUtilisateurAction(Request $request)
-    {
-    	$utilisateur = $this->get('doctrine.orm.entity_manager')
-    	->getRepository('AppBundle:Utilisateur')
-    	->find($request->get('uti_id'));
-    	/* @var $utilisateur Utilisateur */
-    	
-    	$formatted = [
-    			'uti_id' => $utilisateur->getId(),
-               'uti_nom' => $utilisateur->getNom(),
-            	'uti_prenom' => $utilisateur->getPrenom(),
-            	'uti_email' => $utilisateur->getEmail(),
-           		'uti_mot_de_pass' => $utilisateur->getMotDePass(),
-    	];
-    	
-    	return new JsonResponse($formatted);
-    }
+	 * @Route("/utilisateurs", name="utilisateurs_list")
+	 * 
+	 * @method ({"GET"})
+	 */
+	public function getUtilisateursAction(Request $request) {
+		$utilisateurs = $this->get ( 'doctrine.orm.entity_manager' )->getRepository ( 'AppBundle:Utilisateur' )->findAll ();
+		/* @var $utilisateurs Utilisateur[] */
+		
+		$formatted = [ ];
+		foreach ( $utilisateurs as $utilisateur ) {
+			$formatted [] = [ 
+					'uti_id' => $utilisateur->getId (),
+					'uti_nom' => $utilisateur->getNom (),
+					'uti_prenom' => $utilisateur->getPrenom (),
+					'uti_email' => $utilisateur->getEmail (),
+					'uti_mot_de_pass' => $utilisateur->getMotDePass () 
+			];
+		}
+		
+		return new JsonResponse ( $formatted );
+	}
+	
+	/**
+	 * @Route("/utilisateurs/{uti_id}", name="utilisateurs_one")
+	 * 
+	 * @method ({"GET"})
+	 */
+	public function getUtilisateurAction(Request $request) {
+		$utilisateur = $this->get ( 'doctrine.orm.entity_manager' )->getRepository ( 'AppBundle:Utilisateur' )->find ( $request->get ( 'uti_id' ) );
+		/* @var $utilisateur Utilisateur */
+		
+		if (empty($utilisateur)) {
+			return new JsonResponse(['message' => 'Utilisateur non trouve'], Response::HTTP_NOT_FOUND);
+		}
+		
+		$formatted = [ 
+				'uti_id' => $utilisateur->getId (),
+				'uti_nom' => $utilisateur->getNom (),
+				'uti_prenom' => $utilisateur->getPrenom (),
+				'uti_email' => $utilisateur->getEmail (),
+				'uti_mot_de_pass' => $utilisateur->getMotDePass () 
+		];
+		
+		return new JsonResponse ( $formatted );
+	}
 }
